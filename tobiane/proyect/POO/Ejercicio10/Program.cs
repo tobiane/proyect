ae5e680b0a4a5efec3585676a76ad309f44725c7
benjamin <tobiasinzunza@gmail.com>
2024-05-02 14:12:49 -0300

    fe

--- tobiane/proyect/POO/Ejercicio10/Program.cs
+++ tobiane/proyect/POO/Ejercicio10/Program.cs
@@ -1,15 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using System.Security.Cryptography;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
 
-
-namespace Clsee_yObjeto
+namespace Ejercicio10
 {
     public class Copo
     {
